78c82eeea81981b149ba611cfc89ea2012ee9a5b
Fazo01 <fg7829098@gmail.com>
2025-08-16 08:59:45 +0500

    Initialize python Oops

---
+++ A. Class & Object/Question1.py
@@ -0,0 +1,7 @@
+# 1. Create a class `BankAccount` with methods to deposit, withdraw, and check balance. Prevent withdrawals if balance is insufficient.
+class BankAccount:
+  def __init__(self):
+    self.balance=0
+  def deposit(self,amount):
+    amount+=balance
+    
