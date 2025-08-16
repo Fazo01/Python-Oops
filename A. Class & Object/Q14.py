3b0103b8359d14b1a5f4d1e5675f4bddafae7c65
Fazo01 <fg7829098@gmail.com>
2025-08-16 22:12:37 +0500

    Complete Q 14

--- A. Class & Object/Q14.py
+++ A. Class & Object/Q14.py
@@ -1,8 +1,30 @@
 # 14. Write a class `ShoppingCart` that allows adding and removing items, and calculating total price.
 class ShoppingCart:
-  def __init__(self,item,price):
-    self.item={}
-  def remove(self):
-    self.item[item]=price
-    
-  def 
+  def __init__(self):
+    self.items={}
+
+  def add(self,item,price):
+    self.items[item]=price
+    print(f"added {item} with the {price} ")
+  def delete(self,item):
+    if item in self.items:
+      del self.items[item]
+    else:
+      print("No in cart")
+  def CalculateTotalPrice(self):
+    return sum(self.items.values())
+  
+  def show_cart(self):
+    if not self.items:
+      print("Cart is empty")
+    else:
+      print("Shopping Cart")
+      for item,price in self.items.items():
+        print(f"{item} : {price}")
+
+S1=ShoppingCart()
+S1.add("Chips",90)
+S1.add("Chocolate",190)
+S1.add("Orea",30)
+S1.delete("Orea")
+S1.show_cart()
