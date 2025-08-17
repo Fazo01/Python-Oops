54223c1500518f1e6b162313ffed521b06067c99
Fazo01 <fg7829098@gmail.com>
2025-08-17 19:52:10 +0500

    Complete Q 17

---
+++ B. Inheritance & Method Overriding/Q17.py
@@ -0,0 +1,27 @@
+# 17. Implement a class hierarchy for vehicles: `Vehicle` → `Car` → `ElectricCar`.
+class Vehicles():
+  def __init__(self,brand):
+    self.brand=brand
+  def drive(self):
+    print(f"Car brand: {self.brand}")
+
+class Car(Vehicles):
+  def __init__(self, brand,model):
+    self.brand=brand
+    self.model=model
+  def drive(self):
+    print(f"Car brand: {self.brand}. \nCar model: {self.model}")
+
+class ElectricCar(Car):
+  def __init__(self, brand, model,battery):
+    self.brand=brand
+    self.model=model
+    self.battery=battery
+  def drive(self):
+    print(f"Car brand: {self.brand}. \nCar model: {self.model}. \nCar battery: {self.battery}.")
+c1=Vehicles("Toyota")
+c2=Car("Toyota","Grande")
+c3=ElectricCar("Toyota","Grande","M820")
+c1.drive()
+c2.drive()
+c3.drive()
